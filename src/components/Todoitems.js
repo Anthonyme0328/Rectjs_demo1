dd4325f8f6c188666a009d6098426572e22d9e5b
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2020-11-12 16:02:08 -0800

    added color to items on todo

--- src/components/Todoitems.js
+++ src/components/Todoitems.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import propTypes from 'prop-types'
 
 export class Todoitems extends Component {
   render() {
     return (
-      <div>
+      
+      <div style= {{backgroundColor: '#f4f4f4'}}>
         <p>{this.props.todos.title}</p>
       </div>
     )
   }
+}
+
+Todoitems.propTypes = {
+  Todoitems: propTypes.object.isRequired
 }
 
 export default Todoitems
--- src/components/todos.js
+++ src/components/todos.js
@@ -1,13 +1,18 @@
 import { Component } from 'react';
 import Todoitems from './Todoitems'
 import '../App.css';
+import propTypes from 'prop-types'
 
 class Todos extends Component{
   render (){
     return this.props.todos.map((todos) =>
-    <Todoitems todos = {todos}/>
+    <Todoitems key = {todos.id} todos = {todos}/>
     )
   }
+}
+
+Todos.propTypes = {
+  todos: propTypes.array.isRequired
 }
 
 export default Todos;
